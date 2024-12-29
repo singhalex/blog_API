e2a184ced5a433ac07f7fe5a9a4616ea3097a831
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-12-28 21:16:13 -0800

    Add log in and sign up functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,44 @@
 const express = require("express");
+const passport = require("../utils/passport");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // TO-DO
 router.post("/log-in", (req, res, next) => {
-  console.log(`You are logged in ${req}`);
-  res.json({
-    msg: "You are logged in",
-  });
+  passport.authenticate("local", function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      console.log(info.msg);
+      return res.status(401).json(user);
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET);
+    return res.json(token);
+  })(req, res, next);
 });
 
 // TO-DO
 router.post("/sign-up", (req, res, next) => {
-  console.log(`User created: ${req.body.password}`);
-  res.json({
-    msg: "User created",
+  const { username, password, name } = req.body;
+
+  bcrypt.hash(password, 10, async (err, hashedPassword) => {
+    if (err) return next(err);
+
+    try {
+      const user = await prisma.user.create({
+        data: { username, password: hashedPassword, name },
+      });
+      res.json(user);
+    } catch (err) {
+      next(err);
+    }
   });
 });
 
