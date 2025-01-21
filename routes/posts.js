d8c54df74c59ab6d5de790cc6c4e06d8f83577fa
Alex Singh <115970252+singhalex@users.noreply.github.com>
2025-01-21 10:55:03 -0800

    Reorder routes

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,12 @@ router.get("/", postController.posts_get);
 
 router.get("/:postId", postController.single_post_get);
 
+router.post("/", postController.create_post_post);
+
+router.put("/:postId", postController.update_post_post);
+
+router.delete("/:postId", postController.delete_post_post);
+
 router.get("/:postId/comments", (req, res, next) => {
   res.send("THESE ARE THE COMMENTS FOR A POST");
 });
@@ -13,11 +19,5 @@ router.get("/:postId/comments", (req, res, next) => {
 router.post("/:postId/comments", (req, res) => {
   res.send("ADD A COMMENT TO A POST");
 });
-
-router.post("/", postController.create_post_post);
-
-router.put("/:postId", postController.update_post_post);
-
-router.delete("/:postId", postController.delete_post_post);
 
 module.exports = router;
