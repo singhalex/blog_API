8cb628ce24437dd5a920947cdc15153913588248
Alex Singh <115970252+singhalex@users.noreply.github.com>
2025-01-21 10:54:12 -0800

    Change routes to be more RESTful

--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,18 @@ router.get("/", postController.posts_get);
 
 router.get("/:postId", postController.single_post_get);
 
-router.post("/create", postController.create_post_post);
+router.get("/:postId/comments", (req, res, next) => {
+  res.send("THESE ARE THE COMMENTS FOR A POST");
+});
 
-router.post("/:postId/update", postController.update_post_post);
+router.post("/:postId/comments", (req, res) => {
+  res.send("ADD A COMMENT TO A POST");
+});
 
-router.post("/:postId/delete", postController.delete_post_post);
+router.post("/", postController.create_post_post);
+
+router.put("/:postId", postController.update_post_post);
+
+router.delete("/:postId", postController.delete_post_post);
 
 module.exports = router;
