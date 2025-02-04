465af499880ad575e72aeb43a082024e8b4da045
Alex Singh <115970252+singhalex@users.noreply.github.com>
2025-02-03 19:55:53 -0800

    Add cors middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const passport = require("./utils/passport");
 const usersRouter = require("./routes/users");
@@ -18,6 +19,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
